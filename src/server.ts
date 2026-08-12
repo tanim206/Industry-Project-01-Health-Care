0bc7fb8b295e8f936de4a6ed73c5cff542fbacb1
tanim206 <mdtanim.dev@gmail.com>
2026-08-12 23:36:51 +0600

    chore: add test user credentials

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,25 @@
 import app from "./app";
 import config from "./app/config";
 import { prisma } from "./app/lib/prisma";
+import { seedSuperAdmin, seedTesterAdmin, seedTesterDoctor } from "./app/utils/seed";
 
 const PORT = config.port;
 
 const main = async () => {
-	try {
-		await prisma.$connect();
-		console.log("Connected to the database successfully.");
-		app.listen(PORT, () => {
-			console.log(`Server is running on port http://localhost:${PORT}`);
-		});
-	} catch (error) {
-		console.error("Error starting the server:", error);
-		await prisma.$disconnect();
-		process.exit(1);
-	}
+  try {
+    await prisma.$connect();
+    await seedSuperAdmin();
+    await seedTesterAdmin();
+    await seedTesterDoctor();
+    console.log("Connected to the database successfully.");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
 };
 
 main();
